0d112582e09bc9e5278c03e3ef516610e6001a2f
Giovanni Sullutrone <giovisull@gmail.com>
2021-11-22 17:35:03 +0100

    Removed sample comments

--- src/complexcalculator/ComplexCalculator.java
+++ src/complexcalculator/ComplexCalculator.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
- */
 package complexcalculator;
 
 import javafx.application.Application;
@@ -11,10 +7,6 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Gio
- */
 public class ComplexCalculator extends Application {
     
     @Override
@@ -29,11 +21,7 @@ public class ComplexCalculator extends Application {
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
