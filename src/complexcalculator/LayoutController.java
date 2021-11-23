d67c419238a6e39dd61b0cfe315f5a00417bd2c7
SpAreuj <dpicon00@gmail.com>
2021-11-23 11:31:41 +0100

    Initalized stacks and Fixed an error in Layout

--- src/complexcalculator/LayoutController.java
+++ src/complexcalculator/LayoutController.java
@@ -1,10 +1,14 @@
 package complexcalculator;
 
+import Parser.Parser;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
@@ -12,14 +16,43 @@ public class LayoutController implements Initializable {
     @FXML
     private TextField textField;
     @FXML
-    private TableView<?> tableView;
-
+    private TableView<String> tableView;
+    @FXML
+    private TableColumn<String, String> stackColumn;
+    
+    private ObservableList<String> stackList;
+    
+    StackNumber stackNum;
+            
+    StackOperator stackOp;
+    
+    Parser par;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        //Initialize the two stacks
+        StackNumber stackNum = new StackNumber();
+        StackOperator stackOp = new StackOperator(stackNum);
+        
+        //Initialize the Parser
+        //par= new Parser();
+        
+        //Initialize the tableview 
+        tableView = new TableView<>();
+        
+        //Initialize the ObservableList
+        stackList= FXCollections.observableArrayList();       
+        
+        //Initialize the column of the stack
+        stackColumn = new TableColumn<>();
     }
 
     @FXML
     private void enterPressed(ActionEvent event) {
+        //Parse the content of the string
+        //par.parse(textField.getText());
+        
+        //resest the textFileld
+        textField.setText(""); 
     }    
 }
