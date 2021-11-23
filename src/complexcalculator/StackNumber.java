b61b678aa57863e7f00ec57727e11f67881e1aad
Giovanni Sullutrone <giovisull@gmail.com>
2021-11-23 11:50:17 +0100

    Small syntax fix in StackOperator

--- src/complexcalculator/StackNumber.java
+++ src/complexcalculator/StackNumber.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class StackNumber extends LinkedList<Complex> {
     public List<String> getStack(int n_elements) {
         n_elements = (this.size() < n_elements) ? this.size() : n_elements;
-        List<String> listString = new LinkedList<String>();
+        List<String> listString = new LinkedList<>();
         for (Complex c: this.subList(0, n_elements)) {
             listString.add(c.toString());
         }
