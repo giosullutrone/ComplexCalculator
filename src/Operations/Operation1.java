e58f40c91985d0e46282da8ff46516d8792f36c7
Giovanni Sullutrone <giovisull@gmail.com>
2021-11-23 10:40:38 +0100

    Added javadoc to Operation0, Operation1, Operation2

--- src/Operations/Operation1.java
+++ src/Operations/Operation1.java
@@ -2,6 +2,10 @@ package Operations;
 
 import Complex.Complex;
 
+/**
+ * An operation with one Complex input that returns a Complex result.
+ * The Operation1 interface provides one method to execute the associated operation
+ */
 public interface Operation1{
     public Complex execute(Complex a);
 }
