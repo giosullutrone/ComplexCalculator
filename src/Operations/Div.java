e85a86c342a56c720f622cfc001611d2a6f4aaa4
Giovanni Sullutrone <giovisull@gmail.com>
2021-11-23 13:34:40 +0100

    Removed template comments

--- src/Operations/Div.java
+++ src/Operations/Div.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Operations;
 
 import Complex.Complex;
 import javafx.scene.control.Alert;
 import static javax.swing.JOptionPane.showMessageDialog;
 
-/**
- *
- * @author francesca
- */
 public class Div implements Operation2{
 
     @Override
