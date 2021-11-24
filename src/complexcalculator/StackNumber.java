72579e0ed80ffd3ad79fb01ee59fdf115c6a21bf
Giovanni Sullutrone <giovisull@gmail.com>
2021-11-24 09:29:43 +0100

    Fixed bug with StackNumber.getStack

--- src/complexcalculator/StackNumber.java
+++ src/complexcalculator/StackNumber.java
@@ -5,10 +5,13 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class StackNumber extends LinkedList<Complex> {
-    public List<String> getStack(int n_elements) {
-        n_elements = (this.size() < n_elements) ? this.size() : n_elements;
+    public List<String> getStack(int nElements) {
         List<String> listString = new LinkedList<>();
-        for (Complex c: this.subList(0, n_elements)) {
+        
+        if (this.isEmpty() || nElements==0) return listString;
+        
+        nElements = (this.size() < nElements) ? this.size() : nElements;
+        for (Complex c: this.subList(0, nElements)) {
             listString.add(c.toString());
         }
         return listString;
