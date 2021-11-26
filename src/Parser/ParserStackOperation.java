fc5865e78e5cb8b2f1e419205fdfa705e9d18890
Giovanni Sullutrone <giovisull@gmail.com>
2021-11-26 12:04:34 +0100

    Completed ParserStackOperation class

--- src/Parser/ParserStackOperation.java
+++ src/Parser/ParserStackOperation.java
@@ -1,5 +1,10 @@
 package Parser;
 
+import Operations.Clear;
+import Operations.Drop;
+import Operations.Dup;
+import Operations.Over;
+import Operations.Swap;
 import complexcalculator.StackOperator;
 
 public class ParserStackOperation implements ParserInterface{
@@ -16,9 +21,20 @@ public class ParserStackOperation implements ParserInterface{
         s = s.trim().toLowerCase();
         
         switch(s) {
-            case "save":
+            case "Clear":
+                stackOperator.execute(new Clear());
                 return;
-            case "restore":
+            case "Drop":
+                stackOperator.execute(new Drop());
+                return;
+            case "Dup":
+                stackOperator.execute(new Dup());
+                return;
+            case "Over":
+                stackOperator.execute(new Over());
+                return;
+            case "Swap":
+                stackOperator.execute(new Swap());
                 return;
         }
         
