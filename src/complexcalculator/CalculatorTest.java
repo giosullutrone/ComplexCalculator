e936756000e05ca033700044c772d4422ed32b6c
Giovanni Sullutrone <giovisull@gmail.com>
2021-11-26 12:33:06 +0100

    Temp fixed CalculatorTest

--- src/complexcalculator/CalculatorTest.java
+++ src/complexcalculator/CalculatorTest.java
@@ -9,19 +9,19 @@ public class CalculatorTest {
     public static void main(String[] args) {
         StackNumber s = new StackNumber();
         StackOperator m = new StackOperator(s);
-        ParserFactory testing = new ParserFactory(m);
+        // ParserFactory testing = new ParserFactory(m);
         //testing.parse("13+4j");
         
         int n_elements_to_show = 10;
         
-        testing.parse("-10-5j");
+        // testing.parse("-10-5j");
         //m.execute(new Complex(10, 5));
         System.out.println(s.getStack(n_elements_to_show));
         
         m.execute(new Complex(-2, -2));
         System.out.println(s.getStack(n_elements_to_show));
         
-        testing.parse("   +  ");
+        // testing.parse("   +  ");
         System.out.println(s.getStack(n_elements_to_show));
         
         m.execute(new Sqrt());
