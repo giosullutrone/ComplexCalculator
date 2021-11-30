5093f7b54e99bcd289eaf093e574a1bfb503df0c
Giovanni Sullutrone <giovisull@gmail.com>
2021-11-30 17:56:39 +0100

    Used polymorphism in AlertFactory

--- src/AlertMessage/AlertFactory.java
+++ src/AlertMessage/AlertFactory.java
@@ -1,22 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package AlertMessage;
-import javafx.scene.control.Alert;
 
 public class AlertFactory {
-    
-    public AlertMessage operation(OperationException e){
+    public AlertMessage handle(OperationException e){
         return new AlertMessage("Wrong Operation", e.getMessage());
     }
     
-    public AlertMessage syntax(SyntaxException e){
+    public AlertMessage handle(SyntaxException e){
         return new AlertMessage("Wrong Syntax", e.getMessage());
     }
-    
-    
-        
-    
 } 
--- src/AlertMessage/SyntaxException.java
+++ src/AlertMessage/SyntaxException.java
@@ -1,12 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package AlertMessage;
 
-public class SyntaxException  extends RuntimeException{
-    public SyntaxException(String msg){
-        super(msg);
-    } 
-}
+public class SyntaxException  extends RuntimeException{}
