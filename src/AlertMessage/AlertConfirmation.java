b3ede9d500d230940a6a403814a26e4e8e19346b
Francesca Tirino <francesca@MacBook-Pro-di-Francesca.fritz.box>
2021-12-02 16:40:24 +0100

    Add method to return ButtonType

--- src/AlertMessage/AlertConfirmation.java
+++ src/AlertMessage/AlertConfirmation.java
@@ -15,16 +15,20 @@ public class AlertConfirmation {
      * @param headerMessage string representing the Alert's header
      * @param confirmationMessage string representing the Alert's content
      */
-    
+    Alert alert;
     public AlertConfirmation(String headerMessage, String confirmationMessage) {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Confirmation Dialog");
         alert.setHeaderText(headerMessage);
         alert.setContentText(confirmationMessage);
-        Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == ButtonType.OK){
-        // ... user chose OK
-        } 
-     }
-    
     }
+    
+    /**
+     * Method to identify which button is pressed
+     * @return the value of the button is pressed
+     */
+    public ButtonType state (){
+        Optional<ButtonType> result = alert.showAndWait();
+        return result.get();
+    }
+}
