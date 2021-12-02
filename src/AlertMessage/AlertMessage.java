e36c4ae117245995560ae295b3583598c288ef6d
Francesca Tirino <francesca@MacBook-Pro-di-Francesca.fritz.box>
2021-12-02 16:16:26 +0100

    Fix javadoc

--- src/AlertMessage/AlertMessage.java
+++ src/AlertMessage/AlertMessage.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Alert;
 public class AlertMessage {
     /**
      * An object that generates an Aler of type error
-     * @param headerMessage string representing the Alert's heart
+     * @param headerMessage string representing the Alert's header
      * @param errorMessage string representing the Alert's content
      */
     public AlertMessage(String headerMessage, String errorMessage) {
