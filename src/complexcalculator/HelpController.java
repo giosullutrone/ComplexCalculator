54db7a1e20c701fd06d331d83d465a56ffc088ad
francesca2311 <f.tirino@studenti.unisa.it>
2021-12-09 20:00:17 +0100

    Improved readability

--- src/complexcalculator/HelpController.java
+++ src/complexcalculator/HelpController.java
@@ -21,8 +21,8 @@ public class HelpController implements Initializable {
     * Method called on the start of the interface and initialize the operationList,
     * setting to each of them a syntax and a description. The manual is thus created
     *
-    * @param url
-    * @param rb
+    * @param url the url used to resolve relative paths for the root object, or null if the location is not known
+    * @param rb the resource used to localize the root object, or null if the root object was not localized
     *
     */
     @Override
@@ -53,147 +53,147 @@ public class HelpController implements Initializable {
                     
                     //user defined operation
                     case "User defined operation":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "new" + "\n" + "\n"+ 
-                                          "Description:" + "\t" +  "function used to view a pop up that allows you to define a new operation by specifying a name and a sequence of operations. The definition may contain other defined operations. " +
+                        textArea.setText("Syntax:\t\tnew\n\n" + 
+                                          "Description:\tfunction used to view a pop up that allows you to define a new operation by specifying a name and a sequence of operations. The definition may contain other defined operations. " +
                                           "It is possible to delete an operation and modify the definition. \n" + 
                                           "To SAVE to a file the existing defined operations and RELOAD them from a file, you can right mouse click on the operations list. With CLEAR, you can delete all operations list, without modifying file");
                         break;
                     //operation
                     case "Operation | Addition":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "+" + "\n" + "\n"+ 
-                                         "Description:" + "\t" + "function that does the addition of a complex number");
+                        textArea.setText("Syntax:\t\t" + "+" + "\n\n" + 
+                                          "Description:\tfunction that does the addition of a complex number");
                         break;
                     case "Operation | Subtraction":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "-" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function that does the subtraction of a complex number");
+                        textArea.setText("Syntax:\t\t" + "-" + "\n\n" + 
+                                          "Description:\tfunction that does the subtraction of a complex number");
                         break;
                     case "Operation | Multiplication":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "*" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function that does the multiplication of a complex number");
+                        textArea.setText("Syntax:\t\t" + "*" + "\n\n" + 
+                                          "Description:\tfunction that does the multiplication of a complex number");
                         break;
                     case "Operation | Division":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "/" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function that does the division of a complex number");
+                        textArea.setText("Syntax:\t\t" + "/" + "\n\n" + 
+                                         "Description:\tfunction that does the division of a complex number");
                         break;
                     case "Operation | Invert Sign":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "+-" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function that does the invert sign of a complex number");
+                        textArea.setText("Syntax:\t\t" + "+-" + "\n\n" + 
+                                          "Description:\tfunction that does the invert sign of a complex number");
                         break;
                     case "Operation | Square Root":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "sqrt" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function that does the square root of a complex number");
+                        textArea.setText("Syntax:\t\t" + "sqrt" + "\n\n" + 
+                                          "Description:\tfunction that does the square root of a complex number");
                         break;
                     case "Operation | Conjugate":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "conj" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function that does the conjugate of a complex number");
+                        textArea.setText("Syntax:\t\t" + "conj" + "\n\n" + 
+                                         "Description:\tfunction that does the conjugate of a complex number");
                         break;
                         
                     //stack
                     case "Stack operation | Clear":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "clear" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function used to clear the provided Stack");
+                        textArea.setText("Syntax:\t\t" + "clear" + "\n\n" + 
+                                         "Description:\tfunction used to clear the provided Stack");
                         break;
                     case "Stack operation | Swap":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "swap" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function used to swap the top two elements from the provided Stack");
+                        textArea.setText("Syntax:\t\t" + "swap" + "\n\n" + 
+                                         "Description:\tfunction used to swap the top two elements from the provided Stack");
                         break;
                     case "Stack operation | Dup":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "dup" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function used to duplicate the top element from the provided Stack");
+                        textArea.setText("Syntax:\t\t" + "dup" + "\n\n" + 
+                                         "Description:\tfunction used to duplicate the top element from the provided Stack");
                         break;
                     case "Stack operation | Drop":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "drop" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function used to remove the top element from the provided Stack");
+                        textArea.setText("Syntax:\t\t" + "drop" + "\n\n" + 
+                                         "Description:\tfunction used to remove the top element from the provided Stack");
                         break;
                     case "Stack operation | Over":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "over" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function used to duplicate the second element from the top of the provided Stack");
+                        textArea.setText("Syntax:\t\t" + "over" + "\n\n" + 
+                                         "Description:\tfunction used to duplicate the second element from the top of the provided Stack");
                         break;
                         
                     //variables
                     case "Variables | Save":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + ">x" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function used to take the top element from the stack and saves it into the variable \"x\"");
+                        textArea.setText("Syntax:\t\t" + ">x" + "\n\n" + 
+                                         "Description:\tfunction used to take the top element from the stack and saves it into the variable \"x\"");
                         break;
                     case "Variables | Push":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "<x" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function used to push the value of the variable \"x\" onto the stack");
+                        textArea.setText("Syntax:\t\t" + "<x" + "\n\n" + 
+                                         "Description:\tfunction used to push the value of the variable \"x\" onto the stack");
                         break;
                     case "Variables | Addition":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "+x" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function used to take the top element from the stack and adds it to the value of the variable \"x\"");
+                        textArea.setText("Syntax:\t\t" + "+x" + "\n\n" + 
+                                         "Description:\tfunction used to take the top element from the stack and adds it to the value of the variable \"x\"");
                         break;
                     case "Variables | Subtraction":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "-x" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function used to take the top element from the stack and subtracts it from the value of the variable \"x\"");
+                        textArea.setText("Syntax:\t\t" + "-x" + "\n\n" + 
+                                         "Description:\tfunction used to take the top element from the stack and subtracts it from the value of the variable \"x\"");
                         break;
                     case "Variables | Delete":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "!x" + "\n" + "\n"+ 
+                        textArea.setText("Syntax:\t\t" + "!x" + "\n\n" + 
                                          "Description:" + "\t" + "function used to delete the value of the variable \"x\"");
                         break;
                     case "Variables | Delete all":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "clc" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function used to delete the value of all variables ");
+                        textArea.setText("Syntax:\t\t" + "clc" + "\n\n" + 
+                                         "Description:\tfunction used to delete the value of all variables ");
                         break;
                         
                     // stack variables
                     case "Stack Variables | Show":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "vars" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function used to show variables with numbers saved");
+                        textArea.setText("Syntax:\t\t" + "vars" + "\n\n" + 
+                                         "Description:\tfunction used to show variables with numbers saved");
                         break;
                     case "Stack Variables | Save":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "save" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function used to save a copy of all the 26 variables on a variable stack (distinct from the stack used for the operands by the operations)");
+                        textArea.setText("Syntax:\t\t" + "save" + "\n\n" + 
+                                         "Description:\tfunction used to save a copy of all the 26 variables on a variable stack (distinct from the stack used for the operands by the operations)");
                         break;
                     case "Stack Variables | Restore":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "restore" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function used to restore for all variables the last values that were saved on the variable stack (removing them from that stack)");
+                        textArea.setText("Syntax:\t\t" + "restore" + "\n\n" + 
+                                         "Description:\tfunction used to restore for all variables the last values that were saved on the variable stack (removing them from that stack)");
                         break;  
                         
                     //trascendental function
                     case "Trascendental Functions | Modulus/Magnitude":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "mod" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function that does the modulus/magnitude of a complex number");
+                        textArea.setText("Syntax:\t\t" + "mod" + "\n\n" + 
+                                         "Description:\tfunction that does the modulus/magnitude of a complex number");
                         break;
                     case "Trascendental Functions | Argument/Phase":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "arg" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function that does the argument/phase of a complex number");
+                        textArea.setText("Syntax:\t\t" + "arg" + "\n\n" + 
+                                         "Description:\tfunction that does the argument/phase of a complex number");
                         break;
                     case "Trascendental Functions | Natural Logarithm":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "log" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function that does the natural logarithm of a complex number");
+                        textArea.setText("Syntax:\t\t" + "log" + "\n\n" + 
+                                         "Description:\tfunction that does the natural logarithm of a complex number");
                         break;
                     case "Trascendental Functions | Power":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "pow" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function that does the power of a complex number");
+                        textArea.setText("Syntax:\t\t" + "pow" + "\n\n" + 
+                                         "Description:\tfunction that does the power of a complex number");
                         break;
                     case "Trascendental Functions | Sine":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "sin" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function that does the sine of a complex number");
+                        textArea.setText("Syntax:\t\t" + "sin" + "\n\n" + 
+                                         "Description:\tfunction that does the sine of a complex number");
                         break;
                     case "Trascendental Functions | Cosine":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "cos" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function that does the cosine of a complex number");
+                        textArea.setText("Syntax:\t\t" + "cos" + "\n\n" + 
+                                         "Description:\tfunction that does the cosine of a complex number");
                         break;
                     case "Trascendental Functions | Tangent":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "tan" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function that does the tangent of a complex number");
+                        textArea.setText("Syntax:\t\t" + "tan" + "\n\n" + 
+                                         "Description:\tfunction that does the tangent of a complex number");
                         break;
                     case "Trascendental Functions | Arc sine":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "asin" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function that does the arc sine of a complex number");
+                        textArea.setText("Syntax:\t\t" + "asin" + "\n\n" + 
+                                         "Description:\tfunction that does the arc sine of a complex number");
                         break;
                     case "Trascendental Functions | Arc cosine":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "acos" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function that does the arc cosine of a complex number");
+                        textArea.setText("Syntax:\t\t" + "acos" + "\n\n" + 
+                                         "Description:\tfunction that does the arc cosine of a complex number");
                         break;
                     case "Trascendental Functions | Arc tangent":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "atan" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function that does the arc tangent of a complex number");
+                        textArea.setText("Syntax:\t\t" + "atan" + "\n\n" + 
+                                         "Description:\tfunction that does the arc tangent of a complex number");
                         break;
                     case "Trascendental Functions | Exponential":
-                        textArea.setText("Syntax:" + "\t" + "\t"+ "\t" + "exp" + "\n" + "\n"+ 
-                                         "Description:" + "\t" +"function that does the exponential of a complex number");
+                        textArea.setText("Syntax:\t\t" + "exp" + "\n\n" + 
+                                         "Description:\tfunction that does the exponential of a complex number");
                         break;
                     default:
                         textArea.setText("");
