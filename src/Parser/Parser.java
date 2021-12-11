3f586ac40efad33bae911e65339f26f4953b43bf
Giovanni Sullutrone <giovisull@gmail.com>
2021-12-11 12:12:43 +0100

    Added javadoc to ParserSplitterInterface and Parser

--- src/Parser/Parser.java
+++ src/Parser/Parser.java
@@ -4,5 +4,9 @@ package Parser;
  * The Parser interface provides one method to parse the associated string
  */
 public interface Parser {
+    /**
+     * Method used to parse a string into a specific action.
+     * @param s String to parse.
+     */
     public void parse(String s);
 }
--- src/Parser/ParserSplitterInterface.java
+++ src/Parser/ParserSplitterInterface.java
@@ -5,5 +5,10 @@ package Parser;
  * operations (written in a single string)
  */
 public interface ParserSplitterInterface {
+    /**
+     * Method used to parse a string containing multiple operations into a set
+     * of specific actions executed in the provided order.
+     * @param operations String to parse.
+     */
     public void parse(String operations);
 }
